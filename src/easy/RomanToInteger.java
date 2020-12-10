f5a80280bc3753204b1d506e29b5c54959e70585
HowFunLin <1003941051@qq.com>
2020-12-11 00:07:42 +0800

    新增柠檬水找零算法
    Signed-off-by: HowFunLin <1003941051@qq.com>

--- src/easy/RomanToInteger.java
+++ src/easy/RomanToInteger.java
@@ -1,102 +1,78 @@
 package easy;
 
+/**
+ * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。 X 可以放在 L (50) 和 C (100) 的左边，来表示 40
+ * 和 90。  C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。 给定一个罗马数字，将其转换成整数。输入确保在 1
+ * 到 3999 的范围内。
+ * 
+ * @author Riyad
+ *
+ */
 public class RomanToInteger
 {
 	public static void main(String[] args)
 	{
 		RomanToInteger rti = new RomanToInteger();
+		
 		System.out.println(rti.romanToInt("MCMXCIV"));
 	}
 
-	public int romanToInt(String s) {
-        char[] str = s.toCharArray();
-        int len = str.length, number = 0;
-        for(int i=0;i<len;i++){
-            switch(str[i]){
-                case 'M': number+=1000; break;
-                case 'D': number+=500; break;
-                case 'C': 
-                    if(i+1<len && (str[i+1]=='M' || str[i+1]=='D'))
-                        number-=100;
-                    else
-                        number+=100;
-                    break;
-                case 'L': number+=50; break;
-                case 'X': 
-                    if(i+1<len && (str[i+1]=='C' || str[i+1]=='L'))
-                        number-=10;
-                    else
-                        number+=10;
-                    break;
-                case 'V': number+=5; break;
-                case 'I': 
-                    if(i+1<len && (str[i+1]=='V' || str[i+1]=='X'))
-                        number-=1;
-                    else
-                        number+=1;
-                    break;
-            }
-        }
-        return number;
-    }
-	
-	/*
-	public int romanToInt(String s) 
-    {
-        int result = 0;
+	/**
+	 * 罗马数字转整数
+	 * 
+	 * @param s 罗马数字
+	 * @return 罗马数字的整数表示
+	 */
+	public int romanToInt(String s)
+	{
+		char[] str = s.toCharArray();
+		int len = str.length, number = 0;
+
+		for (int i = 0; i < len; i++)
+		{
+			switch (str[i])
+			{
+			
+			case 'M':
+				number += 1000;
+				break;
+				
+			case 'D':
+				number += 500;
+				break;
+				
+			case 'C':
+				if (i + 1 < len && (str[i + 1] == 'M' || str[i + 1] == 'D'))
+					number -= 100;
+				else
+					number += 100;
+				break;
+				
+			case 'L':
+				number += 50;
+				break;
+				
+			case 'X':
+				if (i + 1 < len && (str[i + 1] == 'C' || str[i + 1] == 'L'))
+					number -= 10;
+				else
+					number += 10;
+				break;
+				
+			case 'V':
+				number += 5;
+				break;
+				
+			case 'I':
+				if (i + 1 < len && (str[i + 1] == 'V' || str[i + 1] == 'X'))
+					number -= 1;
+				else
+					number += 1;
+				break;
+			}
+		}
 		
-		for(int i = 0; i < s.length(); i++)
-        {
-        	if(i < s.length() - 1)
-        	{
-        		if(s.charAt(i) == 'I' && s.charAt(i + 1) == 'V') 
-                {
-                    result += 4;
-                    i += 1;
-                    continue;
-                }
-        		if(s.charAt(i) == 'I' && s.charAt(i + 1) == 'X') 
-                {
-                    result += 9;
-                    i += 1;
-                    continue;
-                }
-        		if(s.charAt(i) == 'X' && s.charAt(i + 1) == 'L')
-                {
-                    result += 40;
-                    i += 1;
-                    continue;
-                }
-        		if(s.charAt(i) == 'X' && s.charAt(i + 1) == 'C')
-                {
-                    result += 90;
-                    i += 1;
-                    continue;
-                }
-        		if(s.charAt(i) == 'C' && s.charAt(i + 1) == 'D')
-                {
-                    result += 400;
-                    i += 1;
-                    continue;
-                }
-        		if(s.charAt(i) == 'C' && s.charAt(i + 1) == 'M')
-                {
-                    result += 900;
-                    i += 1;
-                    continue;
-                }
-        	}
-            
-            if(s.charAt(i) == 'I') result += 1;
-        	if(s.charAt(i) == 'V') result += 5;
-        	if(s.charAt(i) == 'X') result += 10;
-        	if(s.charAt(i) == 'L') result += 50;
-        	if(s.charAt(i) == 'C') result += 100;
-        	if(s.charAt(i) == 'D') result += 500;
-        	if(s.charAt(i) == 'M') result += 1000;
-        }
-		
-		return result;
-    }
-    */
+		return number;
+	}
 }

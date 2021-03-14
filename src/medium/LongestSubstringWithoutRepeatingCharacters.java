d8d5c851e42b7f34f7d7f3f4d5502964cb040050
HowFunLin <1003941051@qq.com>
2021-03-14 15:45:19 +0800

    增加三数之和和两两交换链表中的结点算法
    Signed-off-by: HowFunLin <1003941051@qq.com>

--- src/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,32 @@
 package medium;
 
-public class LongestSubstringWithoutRepeatingCharacters {
+import java.util.HashSet;
 
+public class LongestSubstringWithoutRepeatingCharacters
+{
+	public int lengthOfLongestSubstring(String s)
+	{
+		HashSet<Character> set = new HashSet<>();
+		char[] cs = s.toCharArray();
+		int len = 0;
+		int start = 0;
+
+		for (int i = 0; i < cs.length;)
+		{
+			if (!set.add(cs[i]))
+			{
+				len = Math.max(len, i - start);
+				i = s.indexOf(cs[i], start) + 1;
+				start = i;
+				set.clear();
+			} else
+			{
+				if (set.size() > len)
+					len = set.size();
+				i++;
+			}
+		}
+
+		return len == 0 ? s.length() : len;
+	}
 }

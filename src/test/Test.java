2fee8e68813c6e0af011c482a52a5f022a864491
HowFunLin <how-fun@foxmail.com>
2021-08-18 22:32:40 +0800

    剑指 Offer 46. 把数字翻译成字符串
    Signed-off-by: HowFunLin <how-fun@foxmail.com>

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,14 +1,9 @@
 package test;
 
-import java.util.Arrays;
+import medium.TranslateNumbersIntoStrings;
 
 public class Test {
     public static void main(String[] args) {
-        String[] strings = {"10", "2"};
-        Arrays.sort(strings);
-
-        for (String number : strings) {
-            System.out.println(number);
-        }
+        System.out.println(new TranslateNumbersIntoStrings().translateNum(12258));
     }
 }

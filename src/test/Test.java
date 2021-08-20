96aef9239fb21c95f8903dd1b78d3298b9186029
HowFunLin <how-fun@foxmail.com>
2021-08-20 10:50:59 +0800

    541. 反转字符串 II
    Signed-off-by: HowFunLin <how-fun@foxmail.com>

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,9 +1,9 @@
 package test;
 
-import easy.ReverseVowelsOfAString;
+import easy.ReverseStringII;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(new ReverseVowelsOfAString().reverseVowels("aA"));
+        System.out.println(new ReverseStringII().reverseStr("abcdefg", 2));
     }
 }

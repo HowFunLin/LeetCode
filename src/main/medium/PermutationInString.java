8068f67fd5164fe13107470fb55893844057d837
HowFunLin <how-fun@foxmail.com>
2021-11-08 11:15:03 +0800

    567. 字符串的排列
    Signed-off-by: HowFunLin <how-fun@foxmail.com>

--- src/main/medium/PermutationInString.java
+++ src/main/medium/PermutationInString.java
@@ -1,40 +1,58 @@
 package main.medium;
 
-import java.util.HashMap;
-import java.util.Map;
-
+@SuppressWarnings("unused")
 public class PermutationInString {
+    /**
+     * 567. 字符串的排列
+     *
+     * @param s1 字符串
+     * @param s2 字符串
+     * @return s1 的排列之一是否 s2 的 子串
+     */
     public boolean checkInclusion(String s1, String s2) {
-        Map<Character, Integer> s1Map = new HashMap<>();
-        Map<Character, Integer> s2Map = new HashMap<>();
+        int[] c1 = new int[26], c2 = new int[26];
+        int length = s1.length(), size = 0;
 
-        char[] s1Chars = s1.toCharArray();
-        for (char c : s1Chars)
-            s1Map.put(c, s1Map.getOrDefault(c, 0) + 1);
+        for (int i = 0; i < s1.length(); i++)
+            c1[s1.charAt(i) - 'a']++;
+
+        // 计算字母的种数
+        for (int i : c1)
+            size += i > 0 ? 1 : 0;
 
         int left = 0, right = 0, valid = 0;
 
-        char[] s2Chars = s2.toCharArray();
-
+        // 滑动窗口
         while (right < s2.length()) {
-            char c = s2Chars[right];
+            char c = s2.charAt(right);
             right++;
-            if (s1Map.containsKey(c)) {
-                s2Map.put(c, s2Map.getOrDefault(c, 0) + 1);
-                if (s2Map.get(c).equals(s1Map.get(c)))
+            int index = c - 'a';
+
+            // s1中存在该字母
+            if (c1[index] != 0) {
+                c2[index]++;
+
+                // 当字母个数相同时，该字母有效
+                if (c1[index] == c2[index])
                     valid++;
             }
 
-            while (right - left >= s1.length()) {
-                if (valid == s1Map.size())
+            // 窗口长度等于s1时，缩小窗口
+            while (right - left >= length) {
+                // 种类相同则所有个数相同
+                if (valid == size)
                     return true;
 
-                char d = s2Chars[left];
+                c = s2.charAt(left);
                 left++;
-                if (s1Map.containsKey(d)) {
-                    if (s2Map.get(d).equals(s1Map.get(d)))
+                index = c - 'a';
+
+                // 若窗口的左指针符合s1中字母
+                if (c1[index] != 0) {
+                    if (c1[index] == c2[index])
                         valid--;
-                    s2Map.put(d, s2Map.getOrDefault(d, 0) - 1);
+
+                    c2[index]--;
                 }
             }
         }

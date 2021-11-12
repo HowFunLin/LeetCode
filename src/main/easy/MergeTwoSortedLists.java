550ca231f6fe078daff6047e2ca6b66b1103f891
HowFunLin <how-fun@foxmail.com>
2021-11-12 23:10:51 +0800

    21. 合并两个有序链表
    Signed-off-by: HowFunLin <how-fun@foxmail.com>

--- src/main/easy/MergeTwoSortedLists.java
+++ src/main/easy/MergeTwoSortedLists.java
@@ -2,50 +2,27 @@ package main.easy;
 
 import struct.ListNode;
 
-public class MergeTwoSortedLists
-{
-	public ListNode mergeTwoLists(ListNode l1, ListNode l2)
-	{
-		ListNode list;
-		ListNode head = l1;
-		
-		if(l1 == null)
-			return l2;
-		if(l2 == null)
-			return l1;
-		
-		if(l1.val < l2.val)
-		{
-			list = l1;
-			l1 = l1.next;
-		}
-		else
-		{
-			head = list = l2;
-			l2 = l2.next;
-		}
-		
-		while(l1 != null && l2 != null)
-		{
-			if(l1.val < l2.val)
-			{
-				list.next = l1;
-				l1 = l1.next;
-			}
-			else
-			{
-				list.next = l2;
-				l2 = l2.next;
-			}
-			
-			list = list.next;
-		}
-		
-		if(l1 == null)
-			list.next = l2;
-		else
-			list.next = l1;
+@SuppressWarnings("unused")
+public class MergeTwoSortedLists {
+    /**
+     * 21. 合并两个有序链表
+     *
+     * @param l1 升序链表
+     * @param l2 升序链表
+     * @return 拼接给定的两个链表的所有节点组成的新的 升序 链表
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
 
-		return head;
-	}
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
 }

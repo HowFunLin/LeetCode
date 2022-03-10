0ca081667b721ec167bef96443ac2d0165062da5
HowFunLin <how-fun@foxmail.com>
2022-03-10 19:46:05 +0800

    3.8 笔试编程题
    Signed-off-by: HowFunLin <how-fun@foxmail.com>

---
+++ src/company/Shein.java
@@ -0,0 +1,58 @@
+package company;
+
+import struct.ListNode;
+
+import java.util.Scanner;
+
+/**
+ * ACM 模式
+ */
+@SuppressWarnings("unused")
+public class Shein {
+    /**
+     * n：链表的长度，
+     * m：要删除的第 m 个节点
+     * <p>
+     * 字符串：链表每个元素的值，中间用空格隔开
+     */
+    public void test() {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] condition = scanner.nextLine().split(" ");
+        String[] list = scanner.nextLine().split(" ");
+
+        int n = Integer.valueOf(condition[0]), m = Integer.valueOf(condition[1]);
+
+        if (m > n) {
+            System.out.println();
+            System.exit(0);
+        }
+
+        ListNode dummy = new ListNode(), temp = dummy;
+
+        for (String value : list) {
+            temp.next = new ListNode(Integer.valueOf(value));
+            temp = temp.next;
+        }
+
+        temp = dummy;
+
+        for (int i = 1; i < m; i++)
+            temp = temp.next;
+
+        temp.next = temp.next.next;
+
+        StringBuilder sb = new StringBuilder();
+
+        temp = dummy.next;
+
+        while (temp != null) {
+            sb.append(temp.val).append(" ");
+            temp = temp.next;
+        }
+
+        sb.deleteCharAt(sb.length() - 1);
+
+        System.out.println(sb.toString());
+    }
+}

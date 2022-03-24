5a47d6548395b05038f958fb1433608e0cd8b68d
HowFunLin <how-fun@foxmail.com>
2022-03-24 20:54:46 +0800

    3.24 笔试题
    Signed-off-by: HowFunLin <how-fun@foxmail.com>

--- src/company/Shein.java
+++ src/company/Shein.java
@@ -55,4 +55,20 @@ public class Shein {
 
         System.out.println(sb.toString());
     }
+
+    /**
+     * 字符串空格替换为%20
+     */
+    public String changeSpaceTo20(String str) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ')
+                builder.append("%20");
+            else
+                builder.append(str.charAt(i));
+        }
+
+        return builder.toString();
+    }
 }

c9aaad3ac9870541f959ef7f15c3deaccf72ce89
HowFunLin <how-fun@foxmail.com>
2022-04-04 11:51:36 +0800

    105. 从前序与中序遍历序列构造二叉树
    Signed-off-by: HowFunLin <how-fun@foxmail.com>

--- src/main/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -2,31 +2,41 @@ package main.medium;
 
 import struct.TreeNode;
 
-import java.util.Arrays;
-
 @SuppressWarnings("unused")
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    private int pre, in;
+
+    /**
+     * 105. 从前序与中序遍历序列构造二叉树
+     *
+     * @param preorder 二叉树的先序遍历
+     * @param inorder  同一棵树的中序遍历
+     * @return 构造得到的二叉树的根节点
+     */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length == 0 || inorder.length == 0)
-            return null;
+        // 右子树的终止点为不等于树中所有节点的值的节点，即遍历到数组的最后
+        return helper(preorder, inorder, (long) (Integer.MAX_VALUE) + 1);
+    }
 
-        TreeNode root = new TreeNode(preorder[0]);
+    /**
+     * 递归构建二叉树
+     *
+     * @param stop 当前子树在中序遍历数组中结束的下标
+     */
+    private TreeNode helper(int[] preorder, int[] inorder, long stop) {
+        // 下标 in 的值等于 stop 时，stop 为最左节点，已无子节点
+        if (in < inorder.length && inorder[in] != stop) {
+            TreeNode root = new TreeNode(preorder[pre++]);
 
-        for (int i = 0; i < inorder.length; i++) {
-            if (preorder[0] == inorder[i]) {
-                int[] preorderLeft = Arrays.copyOfRange(preorder, 1, i + 1);
-                int[] preorderRight = Arrays.copyOfRange(preorder, i + 1, preorder.length);
+            root.left = helper(preorder, inorder, root.val);
 
-                int[] inorderLeft = Arrays.copyOfRange(inorder, 0, i);
-                int[] inorderRight = Arrays.copyOfRange(inorder, i + 1, inorder.length);
+            in++; // 左子树遍历结束之后，in++ 即为根节点下标
 
-                root.left = buildTree(preorderLeft, inorderLeft);
-                root.right = buildTree(preorderRight, inorderRight);
+            root.right = helper(preorder, inorder, stop);
 
-                break;
-            }
+            return root;
         }
 
-        return root;
+        return null;
     }
 }
